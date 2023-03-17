2a4d55da257e90c832a7934f0175a4fdcc9c7e32
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-03-16 20:07:15 -0400

    app page

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,12 @@ router.get('/profile', requiresAuth(), function (req, res, next) {
     userProfile: JSON.stringify(req.oidc.user, null, 2),
     title: 'Profile page'
   });
+
+router.get('/coverletterai-app', requiresAuth(), function (req, res, next) {
+  res.render('coverletterai-app', {
+    title: 'CoverletterAI App',
+  });
+  
 });
 
 module.exports = router;
